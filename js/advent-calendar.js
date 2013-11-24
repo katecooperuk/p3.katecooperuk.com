325f8dab8def01780de0368cce7fe5eeb174053b
Kate Cooper <kate.cooper@mac.com>
2013-11-24 13:39:59 +0000

    Image Array - console test working but not click listener

--- js/advent-calendar.js
+++ js/advent-calendar.js
@@ -1,9 +1,23 @@
 /*-------------------------------------------------------------------------------------------------
+	Set Up Image Array
+-------------------------------------------------------------------------------------------------*/
+
+ var calendarImg = ["/images/tree.jpg", "/images/santa.jpg", "/images/stockings.jpg"]
+     
+     function imgRandom(imgArr) {
+     	return imgArr[Math.floor(Math.random() * imgArr.length)];
+	 }
+ 
+ console.log(imgRandom(calendarImg));
+ 
+ 
+/*-------------------------------------------------------------------------------------------------
 	Door Open - reveal Background image
 -------------------------------------------------------------------------------------------------*/
 
 $('.doors').click(function(){
 
-	console.log('Door was clicked');
+	//console.log('Door was clicked');
+	$('#dec1').css('background-image', 'calendarImg');
 	
 });
