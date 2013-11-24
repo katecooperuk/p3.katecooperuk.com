4a3555f1e56b281c319cbe83ed4b9a47c52d7265
Kate Cooper <kate.cooper@mac.com>
2013-11-24 14:09:11 +0000

    added new image uploads to array

--- js/advent-calendar.js
+++ js/advent-calendar.js
@@ -2,13 +2,29 @@
 	Set Up Image Array
 -------------------------------------------------------------------------------------------------*/
 
- var calendarImg = ["/images/tree.jpg", "/images/santa.jpg", "/images/stockings.jpg"]
+ var calendarImg = [
+ 	"images/tree.jpg", 
+ 	"images/santa.jpg", 
+ 	"images/stockings.jpg",
+ 	"images/snoopy.jpg",
+ 	"images/stockings2.jpg",
+ 	"images/bear.jpg",
+ 	"images/penguins.jpg",
+ 	"images/baubles.jpg",
+ 	"images/polarbear.jpg",
+ 	"images/village.jpg",
+ 	"images/village2.jpg",
+ 	"images/nativity.jpg",
+ 	"images/santa2.jpg",
+ 	"images/snowman.jpg",
+ 	"images/snow.jpg",
+ 	]
      
      function imgRandom(imgArr) {
      	return imgArr[Math.floor(Math.random() * imgArr.length)];
 	 }
  
- console.log(imgRandom(calendarImg));
+	 console.log(imgRandom(calendarImg));
  
  
 /*-------------------------------------------------------------------------------------------------
