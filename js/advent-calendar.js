f445ddcd88cfb797bf84d0686ef5500c2b9ff307
Kate Cooper <kate.cooper@mac.com>
2013-11-24 14:56:41 +0000

    adding functionality to the first 2 rows - 1-8 Dec

--- js/advent-calendar.js
+++ js/advent-calendar.js
@@ -28,12 +28,55 @@
  
  
 /*-------------------------------------------------------------------------------------------------
-	Door Open - reveal Background image
+	Door Open - select background image from Array
 -------------------------------------------------------------------------------------------------*/
 
-$('.doors').click(function(){
+$('#dec1').click(function(){
 
-	//console.log('Door was clicked');
+	console.log('Door was clicked');
+	
 	$('#dec1').css('background-image', 'calendarImg');
+	
+});
+
+$('#dec2').click(function(){
+	
+	$('#dec2').css('background-image', 'url("images/baubles.jpg")');
+	
+});
+
+$('#dec3').click(function(){
+	
+	$('#dec3').css('background-image', 'url("images/santa.jpg")');
+	
+});
+
+$('#dec4').click(function(){
+	
+	$('#dec4').css('background-image', 'url("images/snowman.jpg")');
+	
+});
+
+$('#dec5').click(function(){
+	
+	$('#dec5').css('background-image', 'url("images/bear.jpg")');
+	
+});
+
+$('#dec6').click(function(){
+	
+	$('#dec6').css('background-image', 'url("images/snow.jpg")');
+	
+});
+
+$('#dec7').click(function(){
+	
+	$('#dec7').css('background-image', 'url("images/santa2.jpg")');
+	
+});
+
+$('#dec8').click(function(){
+	
+	$('#dec8').css('background-image', 'url("images/snoopy.jpg")');
 	
 });
